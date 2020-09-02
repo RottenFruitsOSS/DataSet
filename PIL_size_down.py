54b004a91fb2b24eb45bbcc54e9d5dc6de54a0ed
Jeonyujeong <dbwjd8752@naver.com>
2020-09-02 19:27:18 +0900

    new_path 삭제

--- PIL_size_down.py
+++ PIL_size_down.py
@@ -8,10 +8,7 @@ print(current_path)
 train_path = current_path + '/train_classification/';
 test_path = current_path + '/test_classification/';
 
-new_train_path = current_path + '/train_224/';
-new_test_path = current_path + '/test_224/';
-
-def size_down(image_path, new_path) :
+def size_down(image_path) :
   im = Image.open(image_path)
 
   # resize 이미지
@@ -21,22 +18,19 @@ def size_down(image_path, new_path) :
   print(image_path)
 
 
-# train에 있는 파일 이름 변경
-'''
+# train folder
 os.chdir(train_path)
 for current_dir, dirs, files in os.walk('.'):
   for f in files:
     if f.endswith('.jpg'):
       image_path = train_path + current_dir.lstrip('.\\')  +'/' + f
-      new_path = new_train_path + current_dir.lstrip('.\\')  +'/' + f
-      size_down(image_path, new_path)
-'''
-# test에 있는 파일 이름 변경
+      size_down(image_path)
+
+# test folder
 os.chdir(test_path)
 for current_dir, dirs, files in os.walk('.'):
   for f in files:
     if f.endswith('.jpg'):
       image_path = test_path + current_dir.lstrip('.\\')  +'/' + f
-      new_path = new_test_path + current_dir.lstrip('.\\')  +'/' + f
-      size_down(image_path, new_path)
+      size_down(image_path)
 
